34801376c9281a440008ba403eaeced66dd83bac
Riku Suzuki <r1cky.ft9@gmail.com>
2018-04-05 22:52:20 +0900

    Update create_file.py

--- create_file.py
+++ create_file.py
@@ -4,14 +4,24 @@
 import os
 
 
-def write_texts(content, file_path, file_name):
+def write_contents(contents, file_path, file_name):
 
     # 結果を入れるディレクトリ作成
     if not os.path.exists(file_path):  # フルパスを指定
         os.makedirs(file_path)  # 再帰的作成
     # ファイル開く
-    file = open(file_path + '/' + file_name, 'w')  # ファイルまでのフルパス
-    # 記入
-    file.write(content)
-    file.close()
-    return file
+    with open(file_path + '/' + file_name, 'w') as file_object:  # ファイルまでのフルパス
+        # 記入
+        file_object.write(contents)
+
+
+def append_contents(contents, file_path, file_name):
+
+    # 結果を入れるディレクトリ作成
+    if not os.path.exists(file_path):  # フルパスを指定
+        os.makedirs(file_path)  # 再帰的作成
+    # ファイル開く
+    with open(file_path + '/' + file_name, 'a') as file_object:  # ファイルまでのフルパス
+        # 記入
+        file_object.write(contents)
+
